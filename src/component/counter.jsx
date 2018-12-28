88f95a728a64d87c8ea379ad8828ab8aaac4569d
Apichaya <apichayapg@gmail.com>
2018-12-28 12:32:50 +0700

    test add component

---
+++ src/component/counter.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0
+  };
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <button>Increment</button>
+        <p>hihihi</p>
+        <a href="https://www.facebook.com">facebook</a>
+      </div>
+    );
+  }
+}
+
+export default Counter;
